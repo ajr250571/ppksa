f00b15722bf107dae971664172ccf368940d0ca3
ajr250571 <36195764+ajr250571@users.noreply.github.com>
2022-06-05 21:49:29 -0300

    Casa Async/Await

---
+++ src/routes/async-await.svelte
@@ -0,0 +1,24 @@
+<script>
+	let promesa = getPost();
+	async function getPost() {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		if (response.status === 200) {
+			const resultado = await response.json();
+			return resultado;
+		} else {
+			throw new Error('Error en el request');
+		}
+	}
+</script>
+
+<h1>Async / Await</h1>
+
+{#await promesa}
+	<p>... Cargando</p>
+{:then value}
+	{#each value as item}
+		<p>{item.title}</p>
+	{/each}
+{:catch error}
+	<p>{error.message}</p>
+{/await}
